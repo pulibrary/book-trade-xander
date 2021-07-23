a35c0edda6b39f0431ece5cb70c1df4680a94d3b
Xander Gardner <xandergardner7@gmail.com>
2021-07-23 11:45:29 -0400

    Small rucocop/import changes

--- db/migrate/20210721144844_add_genre_to_books.rb
+++ db/migrate/20210721144844_add_genre_to_books.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddGenreToBooks < ActiveRecord::Migration[6.1]
   def change
     add_column :books, :genre_id, :integer
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -24,4 +25,3 @@ Book.create(title: "Educationed", author: "Tara Weston", description: "Vestibulu
 
 Book.create(title: "Dune", author: "Frank Herbert", description: "Vestibulum pretium accumsan ipsum, nec fringilla erat ultricies ut. Nullam at ipsum id arcu feugiat tincidunt. Nullam nec mi a ligula maximus condimentum. Sed porta nisl neque, non eleifend lorem sollicitudin sit amet. Sed eu velit malesuada, fermentum urna ut, pharetra nisi. Quisque vulputate pellentesque faucibus.", contact_name: "Agatha K", contact_email: "ak@gmail.com", genre: science_fiction)
 Book.create(title: "The Left Hand of Darkness", author: "Ursula", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu malesuada ex, ut dapibus quam. Nullam molestie dui elit, nec volutpat nunc aliquet cursus. Mauris porttitor aliquam ullamcorper", contact_name: "Mike Con", contact_email: "conn@gmail.com", genre: science_fiction)
-
