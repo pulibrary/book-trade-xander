8fed8b0c8f94855f4a54df1a7f0c364388449664
Xander Gardner <xandergardner7@gmail.com>
2021-08-04 11:28:48 -0400

    Added Postgresql and lando

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_30_132218) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
